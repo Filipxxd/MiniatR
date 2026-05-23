3970ca6e77f53849452654b591fa334bf56613c9
filip <filip.chromecka@outlook.com>
2026-05-23 17:06:04 +0200

    add missing test cases

--- MiniatR.Tests/RegistrationTests.cs
+++ MiniatR.Tests/RegistrationTests.cs
@@ -1,5 +1,6 @@
 using AwesomeAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using MiniatR;
 using MiniatR.Extensions;
 using MiniatR.Tests.Fixtures;
 
@@ -19,11 +20,35 @@ public sealed class RegistrationTests
         sender.Should().NotBeNull();
     }
 
+    [Fact]
+    public void ResponseHandler_RegistersCorrectly()
+    {
+        var services = new ServiceCollection();
+        services.AddMiniatR(cfg => cfg.RegisterServicesFromAssemblyContaining<GetUserQuery>());
+        var provider = services.BuildServiceProvider();
+
+        var handler = provider.GetService<IRequestHandler<GetUserQuery, UserResponse>>();
+
+        handler.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void VoidHandler_RegistersCorrectly()
+    {
+        var services = new ServiceCollection();
+        services.AddMiniatR(cfg => cfg.RegisterServicesFromAssemblyContaining<DeleteUserCommand>());
+        var provider = services.BuildServiceProvider();
+
+        var handler = provider.GetService<IRequestHandler<DeleteUserCommand>>();
+
+        handler.Should().NotBeNull();
+    }
+
     [Theory]
     [InlineData(ServiceLifetime.Scoped)]
     [InlineData(ServiceLifetime.Transient)]
     [InlineData(ServiceLifetime.Singleton)]
-    public void Registration_WithLifetime_RegistersCorrectly(ServiceLifetime lifetime)
+    public void Lifetime_RegistersWithCorrectLifetime(ServiceLifetime lifetime)
     {
         var services = new ServiceCollection();
         services.AddMiniatR(cfg => cfg
@@ -36,7 +61,61 @@ public sealed class RegistrationTests
     }
 
     [Fact]
-    public void Registration_NoAssemblies_Throws()
+    public void Transient_NewInstancePerResolve()
+    {
+        var services = new ServiceCollection();
+        services.AddMiniatR(cfg => cfg
+            .RegisterServicesFromAssemblyContaining<GetUserQuery>()
+            .WithHandlerLifetime(ServiceLifetime.Transient));
+        var provider = services.BuildServiceProvider();
+
+        var handler1 = provider.GetRequiredService<IRequestHandler<GetUserQuery, UserResponse>>();
+        var handler2 = provider.GetRequiredService<IRequestHandler<GetUserQuery, UserResponse>>();
+
+        handler1.Should().NotBeSameAs(handler2);
+    }
+
+    [Fact]
+    public void Singleton_SameInstanceAlways()
+    {
+        var services = new ServiceCollection();
+        services.AddMiniatR(cfg => cfg
+            .RegisterServicesFromAssemblyContaining<GetUserQuery>()
+            .WithHandlerLifetime(ServiceLifetime.Singleton));
+        var provider = services.BuildServiceProvider();
+
+        var handler1 = provider.GetRequiredService<IRequestHandler<GetUserQuery, UserResponse>>();
+        var handler2 = provider.GetRequiredService<IRequestHandler<GetUserQuery, UserResponse>>();
+
+        handler1.Should().BeSameAs(handler2);
+    }
+
+    [Fact]
+    public void Scoped_SameWithinScope_DifferentAcrossScopes()
+    {
+        var services = new ServiceCollection();
+        services.AddMiniatR(cfg => cfg
+            .RegisterServicesFromAssemblyContaining<GetUserQuery>()
+            .WithHandlerLifetime(ServiceLifetime.Scoped));
+        var provider = services.BuildServiceProvider();
+
+        IRequestHandler<GetUserQuery, UserResponse> handler1, handler2, handler3;
+        using (var scope1 = provider.CreateScope())
+        {
+            handler1 = scope1.ServiceProvider.GetRequiredService<IRequestHandler<GetUserQuery, UserResponse>>();
+            handler2 = scope1.ServiceProvider.GetRequiredService<IRequestHandler<GetUserQuery, UserResponse>>();
+        }
+        using (var scope2 = provider.CreateScope())
+        {
+            handler3 = scope2.ServiceProvider.GetRequiredService<IRequestHandler<GetUserQuery, UserResponse>>();
+        }
+
+        handler1.Should().BeSameAs(handler2);
+        handler1.Should().NotBeSameAs(handler3);
+    }
+
+    [Fact]
+    public void NoAssemblies_Throws()
     {
         var services = new ServiceCollection();
 
@@ -46,7 +125,7 @@ public sealed class RegistrationTests
     }
 
     [Fact]
-    public async Task Registration_HandlerWithDependencies_ResolvesCorrectly()
+    public async Task HandlerWithDependencies_ResolvesCorrectly()
     {
         var services = new ServiceCollection();
         services.AddScoped<ITestDependency, TestDependency>();
